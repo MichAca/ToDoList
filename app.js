d97ec30de55795c50b93d1736bab43980182b5ca
MichAca <m.aca@outlook.ph>
2019-09-26 10:24:19 +0800

    connect to Mongoose

--- app.js
+++ app.js
@@ -2,74 +2,59 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const items = [];
-const workItems = [];
+// const items = [];
+// const workItems = [];
 
 app.set('view engine', 'ejs'); //should be below const app = express()
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+  name: String
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist"
+});
+
+const item2 = new Item ({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item ({
+  name: "Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
+Item.insertMany(defaultItems, function(err){
+  if(err){
+    console.log(err)
+  } else {
+    console.log("successfully saved default items to DB!")
+  }
+});
+
+
+
 app.get("/", function(req, res) {
 
-  const today = new Date();
-  const day = date.getDate();
+  res.render("list", {listTitle: "Today", newListItems: items});
+
+  //const today = new Date();
+  //const day = date.getDate();
   //  let today = new Date();
-  const currentDay = today.getDate();
+  //const currentDay = today.getDate();
   //  let day = "";
-
-  // if (currentDate === 6 || currentDate === 0){
-  //   day = "weekend";
-  //   //res.sendFile(__dirname + "/weekend.html");
-  //   //res.render("list", {kindOfDay: day});
-  // } else {
-  //   day = "weekday";
-  //   //res.sendFile(__dirname + "/weekday.html");
-  // }
-  // switch (currentDay) {
-
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-  //   case 2:
-  //     day = "Tuesday";
-  //     break;
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-  //   case 6:
-  //     day = "Saturday";
-  //     break;
-  //
-  //   default:
-  //     console.log("Error: Current day is equal to: " + currentDay);
-  // }
-  // let options = {
-  //   weekday: "long",
-  //   day: "numeric",
-  //   month: "long",
-  //   year: "numeric"
-  // };
-  //
-  // day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
-  });
 
 });
 
@@ -87,22 +72,13 @@ app.post("/", function(req, res) {
 });
 
 app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
-  });
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.get("/about", function(req, res) {
   res.render("about");
 });
 
-// app.post("/work", function(req,res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-//
-// });
 
 app.listen(3000, function() {
   console.log("server running on port 3000");
